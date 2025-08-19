1a52afce5b6535c9d6c5ee8411c8a72794d04a72
JorgeJDYFC <jorgejdyfc@gmail.com>
2025-08-18 21:54:29 -0600

    Delete ConversorDeMonedas directory

--- ConversorDeMonedas/src/com/aluracursos/ConversorDeMoneda/Modelo/RespuestaConversion.java
+++
@@ -1,6 +0,0 @@
-package com.aluracursos.ConversorDeMoneda.Modelo;
-
-public record RespuestaConversion(String base_code,
-                                  String target_code,
-                                  double conversion_rate) {
-}
